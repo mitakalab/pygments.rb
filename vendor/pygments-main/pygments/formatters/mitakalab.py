937bbde7879d852c9c53a410d6bd68bbbaa0dd44
Ryo Suzuki <ryooopan@googlemail.com>
2013-01-26 23:29:20 +0900

    fix

--- vendor/pygments-main/pygments/formatters/mitakalab.py
+++ vendor/pygments-main/pygments/formatters/mitakalab.py
@@ -88,16 +88,14 @@ class MitakalabFormatter(Formatter):
 			points = []
 			lines = []
 			for i in range(fl, fl+lncount):
-				points.append('<span id="P%d" rel="P%d" class="point">' % (i, i) + '○</span>')
+				points.append('<span id="P%d" rel="P%d" class="point">' % (i, i) + '-</span>')
 				
 				lines.append('<span id="L%d" rel="L%d" class="number">' % (i, i) + '%*d' % (mw, i) + '</span>')
 			lp = '\n'.join(points)
 			ls = '\n'.join(lines)
 
-			yield '<table class="points"><tr>'
-			yield '<td class="line_points">' + lp + '</td>'
-			yield '</tr></table>'
 			yield '<table class="lines highlight"><tr>'
+			yield '<td class="line_points">' + lp + '</td>'
 			yield '<td class="line_numbers">' + ls + '</td>'
 			yield '<td class="line_data">'
 			yield dummyoutfile.getvalue()
